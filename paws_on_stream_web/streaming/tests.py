876f9f8bb5f9b4835e9cba7583465e6897d12931
Daniel Bacher <daniel.bacher@kit.edu>
2026-06-10 14:33:58 +0200

    add tests for the message model

--- paws_on_stream_web/streaming/tests.py
+++ paws_on_stream_web/streaming/tests.py
@@ -1,9 +1,11 @@
-from datetime import UTC, datetime, timedelta, timezone
+import uuid
+from datetime import UTC, datetime, timedelta
 
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from streaming.factories import EventFactory
-from streaming.models import Event
+from streaming.factories import EventFactory, TextMessageFactory
+from streaming.models import Event, Message
 
 
 # Create your tests here.
@@ -31,3 +33,44 @@ class EventModelTest(TestCase):
         older = EventFactory(starts_at=datetime.now(tz=UTC) + timedelta(days=1))
         qs = Event.objects.all()
         self.assertEqual(list(qs), [recent, older])
+
+
+class MessageModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.message = TextMessageFactory()
+
+    def test_media_type_choices(self):
+        for choice, _ in Message.MEDIA_TYPES:
+            self.message.media_type = choice
+            self.message.full_clean()
+        self.message.media_type = "invalid_choice"
+        with self.assertRaises(ValidationError):
+            self.message.full_clean()
+
+    def test_status_choices(self):
+        for status, _ in Message.STATUS_CHOICES:
+            self.message.status = status
+            self.message.full_clean()
+        self.message.status = "garbage"
+        with self.assertRaises(ValidationError):
+            self.message.full_clean()
+
+    def test_rejection_reason_choices(self):
+        for reason, _ in Message.REJECTION_REASONS:
+            self.message.rejection_reason = reason
+            self.message.full_clean()
+        self.message.rejection_reason = "unknown_reason"
+        with self.assertRaises(ValidationError):
+            self.message.full_clean()
+
+    def test_uuid_is_generated(self):
+        m = TextMessageFactory()
+        self.assertIsNotNone(m.id)
+        uuid.UUID(m.id)
+
+    def test_str_is_human_readable(self):
+        self.assertEqual(
+            str(self.message),
+            f"{self.message.participant.display_name}: {self.message.content[:50]}...",
+        )
