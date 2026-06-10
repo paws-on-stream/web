fc6e30a29ef691e4dcbcbe4d73d8f6e767910e5e
Daniel Bacher <daniel.bacher@kit.edu>
2026-06-10 14:25:18 +0200

    add tests for the event model

--- paws_on_stream_web/streaming/tests.py
+++ paws_on_stream_web/streaming/tests.py
@@ -1,3 +1,33 @@
+from datetime import UTC, datetime, timedelta, timezone
+
 from django.test import TestCase
 
+from streaming.factories import EventFactory
+from streaming.models import Event
+
+
 # Create your tests here.
+class EventModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.event = EventFactory()
+
+    def test_defaults(self):
+        e = self.event
+        self.assertFalse(e.is_active)
+        self.assertTrue(e.allow_messages)
+        self.assertEqual(e.display_mode, "")
+        self.assertIsNone(e.scroll_speed_px)
+
+    def test_str_is_human_readable(self):
+        self.assertEqual(
+            str(self.event),
+            f"{self.event.name} ({self.event.starts_at.strftime('%Y-%m-%d %H:%M')})",
+        )
+
+    def test_default_ordering_desc_by_starts_at(self):
+        Event.objects.all().delete()
+        recent = EventFactory(starts_at=datetime.now(tz=UTC) + timedelta(days=5))
+        older = EventFactory(starts_at=datetime.now(tz=UTC) + timedelta(days=1))
+        qs = Event.objects.all()
+        self.assertEqual(list(qs), [recent, older])
