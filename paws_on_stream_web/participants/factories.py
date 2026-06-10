5be2c3ec4ee41fe7d9ee8c5fc2af50e66b95f2de
Daniel Bacher <daniel.bacher@kit.edu>
2026-06-10 11:54:56 +0200

    fix factory

--- paws_on_stream_web/participants/factories.py
+++ paws_on_stream_web/participants/factories.py
@@ -2,6 +2,8 @@ import factory
 
 from participants.models import Participant
 
+TELEGRAM_ID_MAX = 9_223_372_036_854_775_807
+
 
 class ParticipantFactory(factory.django.DjangoModelFactory):
     """Erzeugt ein Participant-Objekt mit sinnvollen Default-Werten."""
@@ -11,4 +13,4 @@ class ParticipantFactory(factory.django.DjangoModelFactory):
         django_get_or_create = ("telegram_id",)
 
     display_name = factory.Faker("name")
-    telegram_id = factory.Faker("random_number", digits=19)
+    telegram_id = factory.Sequence(lambda n: TELEGRAM_ID_MAX - n)
